94c430b2fd571bf76a4b2065e31fb807d3d17214
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2023-12-23 14:22:30 +0530

    Time: 2 ms (98.87%) | Memory: 59.7 MB (96.97%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -1,78 +1,38 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-      int cnt = 0;
-        // Process 'G'
-        int gind = 0;
-    
+        int totaltime = 0;
+
         for (int i = 0; i < garbage.length; i++) {
-           
-                if (garbage[i].contains("G")) {
-                    int occr = fun(garbage[i], 'G');
-                    cnt += occr;
-                    gind = i;
-                }
-        }
-        if(gind>0)
-        {
-            int travelg = funn(travel,gind);
-            cnt+=travelg;
-        }
-       
-        // Process 'P'
-        int pind = 0;
-        for (int i = 0; i < garbage.length; i++) {
-            
-                if (garbage[i].contains("P")) {
-                    int occr = fun(garbage[i], 'P');
-                    cnt += occr;
-                    pind = i;
-                }
-        }
-        if(pind>0)
-        {
-            int travelg = funn(travel,pind);
-            cnt+=travelg;
-        }
+            totaltime += garbage[i].length();
 
-        // Process 'M'
-        int mind = 0;
-        for (int i = 0; i < garbage.length; i++) {
-            
-                if (garbage[i].contains("M")) {
-                    int occr = fun(garbage[i], 'M');
-                    cnt += occr;
-                    mind = i;
-                }
         }
-        if(mind>0)
-        {
-            int travelg = funn(travel,mind);
-            cnt+=travelg;
+        for (int x : travel) {
+            totaltime += (x * 3);
         }
-
-        return cnt;
-    }
-
-    public int fun(String s, char c) {
-        int tempcnt = 0;
-        char[] charArray = s.toCharArray();
-
-        for (char ch : charArray) {
-            if (ch == c) {
-                tempcnt++;
+        for (int i = garbage.length - 1; i > 0; i--) {
+            if (garbage[i].indexOf("G") == -1) {
+                totaltime -= travel[i - 1];
+            } else {
+                break;
             }
-        }
-
-        return tempcnt;
-    }
-    public int funn(int [] arr,int ind)
-    {
-        int tcnt = 0;
-        for(int i = 0;i<ind;i++)
-        {
-            tcnt+=arr[i];
 
         }
-        return tcnt;
+        for (int i = garbage.length - 1; i > 0; i--) {
+            if (garbage[i].indexOf("P") == -1) {
+                totaltime -= travel[i - 1];
+            } else {
+                break;
+            }
+
+        }
+        for (int i = garbage.length - 1; i > 0; i--) {
+            if (garbage[i].indexOf("M") == -1) {
+                totaltime -= travel[i - 1];
+            } else {
+                break;
+            }
+
+        }
+        return totaltime;
     }
 }
