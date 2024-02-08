e3e2ea8e305b1d251b50ba17aea998ccc186d6eb
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-02-08 22:19:48 +0530

    Time: 15 ms (11.37%) | Memory: 48 MB (5.24%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
-        //ans=getOverlapping(intervals);
-         Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-         for(int i=0;i<intervals.length;i++)
-         {
-            if(ans.size()>0 && ans.get(ans.size()-1).get(1)>=intervals[i][0])
-            {
-                ans.get(ans.size()-1).set(1, Math.max(ans.get(ans.size()-1).get(1),intervals[i][1]));
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        for(int i=0;i<intervals.length;i++){
+
+            if(ans.size()!=0 && ans.get(ans.size()-1).get(1)>= intervals[i][0]){
+                ans.get(ans.size()-1).set(1,Math.max(intervals[i][1],ans.get(ans.size()-1).get(1)));
             }
             else{
                 ArrayList<Integer> temp=new ArrayList<>();
@@ -15,13 +13,16 @@ class Solution {
                 temp.add(intervals[i][1]);
                 ans.add(temp);
             }
-         }
-         int s[][]=new int [ans.size()][2];
+
+        }
+        System.out.println(ans);
+        int s[][]=new int [ans.size()][2];
          for(int i=0;i<s.length;i++)
          {
              s[i][0]=ans.get(i).get(0);
              s[i][1]=ans.get(i).get(1);
          }
          return s;
+
     }
 }
