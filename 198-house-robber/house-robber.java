443bf058148e85868f9396031591cbb27c145e78
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-02-11 17:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (85.93%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -6,11 +6,19 @@ class Solution {
     }
 
     private static int helper(int[] nums, int n,int [] dp) {
-        if(n<0) return 0;
-        if(n==0) return dp[n]=nums[0];
-        if(dp[n] != -1 )return dp[n];
-        int pick=nums[n]+helper(nums, n-2,dp);
-        int nonpick=helper(nums, n-1,dp);
-        return dp[n]=Math.max(pick, nonpick);
+        // if(n<0) return 0;
+        // if(n==0) return dp[n]=nums[0];
+        // if(dp[n] != -1 )return dp[n];
+        // int pick=nums[n]+helper(nums, n-2,dp);
+        // int nonpick=helper(nums, n-1,dp);
+        // return dp[n]=Math.max(pick, nonpick);
+
+        dp[0]=nums[0];
+        if(n>=1)
+            dp[1]=Math.max(nums[0], nums[1]);
+        for(int i=2;i<=n;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n];
     }
 }
