64adf47bdfd41d26411bf4cd6afb49e5d31b5145
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-02-12 22:42:17 +0530

    Time: 5 ms (12.89%) | Memory: 45.6 MB (38.10%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -6,8 +6,33 @@ class Solution {
     // Calculate and print the minimum sum path in the matrix
     int dp[][] = new int[n][m];
 
+   // int dp[][] = new int[n][m];
+
     for (int[] row : dp) Arrays.fill(row, -1);
-    return(minSumPath(n - 1, m - 1, matrix, dp));
+    //System.out.println(minSumPath(n - 1, m - 1, matrix, dp));
+    //tabulation
+    dp[0][0] = matrix[0][0];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (i == 0 && j == 0) {
+          dp[0][0] = matrix[0][0];
+          continue;
+        }
+        int up = matrix[i][j];
+                    if (i > 0)
+                        up += dp[i - 1][j]; // Add the value from above if it's not out of bounds
+                    else
+                        up += (int) Math.pow(10, 9); // Add a large value if out of bounds in the up direction
+
+                    int left = matrix[i][j];
+                    if (j > 0)
+                        left += dp[i][j - 1]; // Add the value from the left if it's not out of bounds
+                    else
+                        left += (int) Math.pow(10, 9); 
+        dp[i][j] = Math.min(up, left);
+      }
+    }
+    return(dp[n - 1][m - 1]);
   }
 
   private static int minSumPath(int n, int m, int[][] matrix, int[][] dp) {
