3338dc3e55cab049d080c143aa27e9c38d9ab1c4
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-02-17 13:50:01 +0530

    Time: 6 ms (80.06%) | Memory: 44.2 MB (54.03%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,11 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] arr) {
-        int n=arr.length;
-        int dp[][]=new int[n][n+1];//strt from -1 and goes  till n-1;
-        for (int row[] : dp) {
-            Arrays.fill(row, -1);
+    //     int n=arr.length;
+    //     int dp[][]=new int[n][n+1];//strt from -1 and goes  till n-1;
+    //     for (int row[] : dp) {
+    //         Arrays.fill(row, -1);
+    //     }
+    //    return helper(arr,0,(int)-1,dp);
+     ArrayList<Integer> ar=new ArrayList<>();
+        ar.add(arr[0]);
+       int len=1;
+       for(int i=1;i<arr.length;i++)
+       {
+        if(arr[i]>ar.get(ar.size()-1)){
+            ar.add(arr[i]);
+            len++;
         }
-       return helper(arr,0,(int)-1,dp);
+        else{
+           // int id=Collections.binarySearch(ar, arr[i]);
+            
+            int ind=Collections.binarySearch(ar, arr[i]);
+            if (ind < 0) {
+                ind = -ind - 1;
+            }
+            ar.set(ind,arr[i]);
+        }
+       }
+       return len;
     }
 
     private static int helper(int[] arr, int n, int prev,int dp[][]) {
