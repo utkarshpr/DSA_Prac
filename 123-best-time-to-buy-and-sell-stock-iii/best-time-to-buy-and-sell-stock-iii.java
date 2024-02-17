ac99dae1a443053910a5f5706b5f123b24205679
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-02-17 08:15:43 +0530

    Time: 65 ms (38.61%) | Memory: 63.9 MB (30.01%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,54 +1,33 @@
 class Solution {
-    public int maxProfit(int[] Arr) {
-        int n = Arr.length;
-        int[][][] dp = new int[n][2][3];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
-        }
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        // int[][][] dp = new int[n][2][3];
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < 2; j++) {
+        //         Arrays.fill(dp[i][j], -1);
+        //     }
+        // }
         // for(int x[]:dp)
         //     Arrays.fill(x, -1);
         // int profit = 0;
-        // int dp[][] = new int[n + 1][2 + 1];
-        // dp[n][0] = dp[n][1] = 0;
-        // for (int ind = n - 1; ind >= 0; ind--) {
-        //     for (int buy = 0; buy <= 1; buy++) {
-        //         if (buy == 0) { // We can buy the stock
-        //             profit = Math.max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
-        //         }
-    
-        //         if (buy == 1) { // We can sell the stock
-        //             profit = Math.max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 1][0]);
-        //         }
-    
-        //         dp[ind][buy] = profit;
-        //   }
-        // }
-        // return dp[0][0];
-        return helper(Arr,0,0,2,dp);
-        
-      }
-    
-      private static int helper(int[] Arr, int n, int _buy,int cap, int dp[][][]) {
-        if(cap == 0) return 0;
-        if (n >= Arr.length) return 0;
-        int profit;
-        if (dp[n][_buy][cap] != -1) return dp[n][_buy][cap];
-        if (_buy == 0) {
-          profit =
-            Math.max(
-              -Arr[n] + helper(Arr, n + 1, 1,cap, dp),
-              0 + helper(Arr, n + 1, 0,cap, dp)
-            );
-        } else {
-          profit =
-            Math.max(
-              Arr[n] + helper(Arr, n + 1, 0,cap-1, dp),
-              0 + helper(Arr, n + 1, 1,cap, dp)
-            );
+         int dp[][][] = new int[n + 1][2][3];
+      //   dp[n][0] = dp[n][1] = 0;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+
+                    if (buy == 0) { // We can buy the stock
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][0][cap],
+                                -prices[ind] + dp[ind + 1][1][cap]);
+                    }
+
+                    if (buy == 1) { // We can sell the stock
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][1][cap],
+                                prices[ind] + dp[ind + 1][0][cap - 1]);
+                    }
+                }
+            }
         }
-        return dp[n][_buy][cap]=profit;
-      }
+        return dp[0][0][2];
     }
-    
+}
